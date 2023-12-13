81a996ddb0289307ea4f15a08167164b4f0044e6
natmfat <natmfat@gmail.com>
2023-12-13 10:57:01 -0800

    [🎨] Propagating Peaks

--- shared/utils/grid.ts
+++ shared/utils/grid.ts
@@ -1,8 +1,8 @@
 export function createGrid(count: number) {
   const grid: number[][] = [];
 
-  for (let x = 0; x < count; x++) {
-    for (let y = 0; y < count; y++) {
+  for (let y = 0; y < count; y++) {
+    for (let x = 0; x < count; x++) {
       grid.push([
         count <= 1 ? 0.5 : x / (count - 1),
         count <= 1 ? 0.5 : y / (count - 1),
