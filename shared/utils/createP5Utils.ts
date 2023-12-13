4d834e9b3dee2b65fe9304fe9c19102e405740bc
natmfat <natmfat@gmail.com>
2023-12-13 13:45:19 -0800

    [✨] Update Scale Utils

--- shared/utils/createP5Utils.ts
+++ shared/utils/createP5Utils.ts
@@ -1,6 +1,19 @@
 import p5 from "p5";
 
-export function createP5Utils(sketch: p5) {
+export type P5UtilsConfig = {
+  gridSizeX: number;
+  gridSizeY: number;
+  gridMargin: number;
+};
+
+export function createP5Utils(
+  sketch: p5,
+  { gridSizeX = 0, gridSizeY = 0, gridMargin = 40 }: Partial<P5UtilsConfig> = {}
+) {
+  /**
+   * Add an outline built-in to the canvas instead of a CSS outline
+   * @param weight Width of the outline
+   */
   function frame(weight: number) {
     const doubleWeight = weight * 2;
     const offset = 0.5;
@@ -16,9 +29,13 @@ export function createP5Utils(sketch: p5) {
     );
   }
 
-  function scale(gridSize: number, gridMargin: number, i: number) {
-    return sketch.lerp(gridMargin, gridSize - gridMargin, i);
+  function scaleX(x: number) {
+    return sketch.lerp(gridMargin, gridSizeX - gridMargin, x);
   }
 
-  return { frame, scale };
+  function scaleY(x: number) {
+    return sketch.lerp(gridMargin, gridSizeY - gridMargin, x);
+  }
+
+  return { frame, scaleX, scaleY };
 }
